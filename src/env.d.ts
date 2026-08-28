a8d444a3c9fbd90a7d2bd58d9dbb153b445623f0
Sudhir <sudhirdubey558@gmail.com>
2026-08-28 14:31:13 +0530

    feat: improve writing discovery and remove comments

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,10 +3,6 @@
 
 interface ImportMetaEnv {
 	readonly PUBLIC_GA_MEASUREMENT_ID?: string;
-	readonly PUBLIC_GISCUS_REPO?: string;
-	readonly PUBLIC_GISCUS_REPO_ID?: string;
-	readonly PUBLIC_GISCUS_CATEGORY?: string;
-	readonly PUBLIC_GISCUS_CATEGORY_ID?: string;
 }
 
 interface ImportMeta {
